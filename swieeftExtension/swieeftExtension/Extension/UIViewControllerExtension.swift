0bc10e40c91a8ca766eda5bdf6b0bbe6e3b93afc
GilNam Park <swieeft89@gmail.com>
2019-12-09 11:14:49 +0900

    UIViewController Extension에 Toast 메시지 기능 추가

--- swieeftExtension/swieeftExtension/Extension/UIViewControllerExtension.swift
+++ swieeftExtension/swieeftExtension/Extension/UIViewControllerExtension.swift
@@ -41,3 +41,18 @@ extension UIViewController: StoryboardName, ViewControllerName {
     }
 }
 
+extension UIViewController {
+    func showToast(message : String, seconds: Double = 2.0) {
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        alert.view.backgroundColor = .black
+        alert.view.alpha = 0.6
+        alert.view.layer.cornerRadius = 15
+        
+        self.present(alert, animated: true)
+        
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + seconds) {
+            alert.dismiss(animated: true)
+        }
+    }
+    
+}
